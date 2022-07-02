9cb41a5a839b4ec2f50eb5620028e46db1f4a771
deepdata21 <deepdata.kdijw@slmail.me>
2022-07-02 12:19:28 -0500

    Customer microservice added

---
+++ README.md
@@ -0,0 +1,3 @@
+# microservices-project
+
+- Project carried out as part of Microservices and Distributed System course offered by Amigoscode
---
+++ customer/src/main/java/com/debajyoti/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.debajyoti.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
